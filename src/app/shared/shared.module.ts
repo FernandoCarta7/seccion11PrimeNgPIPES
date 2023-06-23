b77a5f45ba9cec63281a51c6b57222af53dcef75
Fernando Carta <cartafernando@outlook.com>
2023-06-23 10:16:17 -0500

    Commit inicial

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { MenuItem } from 'primeng/api';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './components/menu/menu.component';
+
+import { PrimeNgModule, } from '../prime-ng/prime-ng.module';
+
+
+
+@NgModule({
+  declarations: [
+    MenuComponent
+  ],
+  imports: [
+    CommonModule,
+    PrimeNgModule,
+
+  ],
+  exports:[
+    MenuComponent
+  ]
+})
+export class SharedModule { }
